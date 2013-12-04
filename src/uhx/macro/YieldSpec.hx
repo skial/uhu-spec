bea501266d1c537f33815d90d7df86c258fa7d4d
Skial Bainn <skial.bainn@gmail.com>
2013-12-04 09:59:46 +0000

    Each test goes through a manual call, a for loop and while loop.

--- src/uhx/macro/YieldSpec.hx
+++ src/uhx/macro/YieldSpec.hx
@@ -13,15 +13,75 @@ class YieldSpec implements Klas {
 		
 	}
 	
-	public function testTicker_manual() {
+	public function testTicker_simple_manual() {
 		var count = ticker();
 		Assert.equals(1, count.next());
 		Assert.equals(2, count.next());
 	}
 	
-	public function testTicker_for() {
-		var values = [for (t in ticker()) t];
+	public function testTicker_simple_for() {
+		var tick = ticker();
+		var values = [for (t in tick) t];
 		Assert.equals('' + [1, 2, 3, 4, 5], '' + values);
+		Assert.equals(false, tick.hasNext());
+	}
+	
+	public function testTicker_simple_while() {
+		var values = [];
+		var tick = ticker();
+		while (tick.hasNext()) values.push( tick.next() );
+		Assert.equals('' + [1, 2, 3, 4, 5], '' + values);
+		Assert.equals(false, tick.hasNext());
+	}
+	
+	public function testLoop_range_manual() {
+		var range = range();
+		Assert.equals(0, range.next());
+		Assert.equals(1, range.next());
+	}
+	
+	public function testLoop_range_for() {
+		var range = range();
+		var values = [for (r in range) r];
+		Assert.equals('' + [0, 1, 2, 3, 4], '' + values);
+		Assert.equals(false, range.hasNext());
+	}
+	
+	public function testLoop_range_while() {
+		var values = [];
+		var range = range();
+		while (range.hasNext()) values.push( range.next() );
+		Assert.equals('' + [0, 1, 2, 3, 4], '' + values);
+		Assert.equals(false, range.hasNext());
+	}
+	
+	public function testLoop_multi_range_manual() {
+		var range = multi_range();
+		Assert.equals(1, range.next());
+		Assert.equals(2, range.next());
+		Assert.equals(3, range.next());
+	}
+	
+	public function testLoop_multi_range_for() {
+		var range = multi_range();
+		var values = [for (r in range) r];
+		Assert.equals('' + [1, 2, 3, 2, 3, 4], '' + values);
+		Assert.equals(false, range.hasNext());
+	}
+	
+	public function testLoop_multi_range_while() {
+		var values = [];
+		var range = multi_range();
+		while (range.hasNext()) values.push( range.next() );
+		Assert.equals('' + [1, 2, 3, 2, 3, 4], '' + values);
+		Assert.equals(false, range.hasNext());
+	}
+	
+	public function testArg_manual() {
+		var range = arg(2);
+		Assert.equals(0, range.next());
+		Assert.equals(2, range.next());
+		Assert.equals(4, range.next());
 	}
 	
 	public function ticker() {
@@ -35,6 +95,26 @@ class YieldSpec implements Klas {
 		@:yield return 4;
 		trace( 'Goodbye World' );
 		@:yield return 5;
+	}
+	
+	public function range() {
+		for (i in 0...5) {
+			@:yield return i;
+		}
+	}
+	
+	public function multi_range() {
+		for (i in 0...2) {
+			@:yield return i + 1;
+			@:yield return i + 2;
+			@:yield return i + 3;
+		}
+	}
+	
+	public function arg(v:Int) {
+		for (i in 0...5) {
+			@:yield return v * i;
+		}
 	}
 	
 }
