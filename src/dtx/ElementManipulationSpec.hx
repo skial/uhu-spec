3a0e0b46ea8a01ca234ad834197b32cac7f65970
Skial Bainn <skial.bainn@gmail.com>
2014-11-03 13:30:55 +0000

    Add conditional statements.

--- src/dtx/ElementManipulationSpec.hx
+++ src/dtx/ElementManipulationSpec.hx
@@ -297,7 +297,11 @@ class ElementManipulationSpec
 
 	public function testValOnComment():Void 
 	{
+		#if sys
+		Assert.equals("A comment", comment.val());
+		#else
 		Assert.equals(" A comment ", comment.val());
+		#end
 	}
 
 	public function testValOnTextNode():Void 
@@ -390,7 +394,11 @@ class ElementManipulationSpec
 
 	public function testInnerHTMLOnNonElements():Void 
 	{
+		#if sys
+		Assert.equals("A comment", comment.innerHTML());
+		#else
 		Assert.equals(" A comment ", comment.innerHTML());
+		#end
 		Assert.equals("Text", text.innerHTML());
 	}
 
@@ -458,7 +466,11 @@ class ElementManipulationSpec
 
 	public function testHtmlOnNonElements():Void 
 	{
+		#if sys
+		Assert.equals("<!--A comment-->", comment.html());
+		#else
 		Assert.equals("<!-- A comment -->", comment.html());
+		#end
 		Assert.equals("Text", text.html());
 	}
 
