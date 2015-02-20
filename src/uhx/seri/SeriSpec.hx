d3c382d6c998b76a39035e00b46b94222c9fa28d
Skial Bainn <skial.bainn@gmail.com>
2015-02-20 14:45:08 +0000

    Add src/uhx/seri/SeriSpec.hx

---
+++ src/uhx/seri/SeriSpec.hx
@@ -0,0 +1,24 @@
+package uhx.seri;
+
+import uhx.sys.Seri;
+import utest.Assert;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class SeriSpec {
+
+	public function new() {
+		
+	}
+	
+	public function testVersion() {
+		Assert.equals( '7.0.0', Seri.version );
+	}
+	
+	public function testCategory() {
+		var codepoints = Seri.getCategory( 'Zs' );
+	}
+	
+}
