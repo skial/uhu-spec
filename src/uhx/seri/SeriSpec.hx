3d036aa325e4120f896b71e2d44c1dff7fa2ab98
Skial Bainn <skial.bainn@gmail.com>
2015-03-05 14:28:18 +0000

    Add block tests.

--- src/uhx/seri/SeriSpec.hx
+++ src/uhx/seri/SeriSpec.hx
@@ -52,6 +52,35 @@ class SeriSpec {
 		Assert.isTrue( codepoints1.indexOf( 0xFF1B ) > -1 );
 		Assert.isTrue( codepoints1.indexOf( 0x11049 ) > -1 );
 		Assert.isTrue( codepoints1.indexOf( 0x1BC9F ) > -1 );
+		
+		var category = 'Po';
+		var codepoints2 = Seri.getCategory( category );
+	}
+	
+	public function testBlocks() {
+		var blockpoints0 = Seri.getBlock( 'Basic Latin' );
+		
+		Assert.equals( 128, blockpoints0.length );
+		Assert.isTrue( blockpoints0.indexOf( 0x0000 ) > -1 );
+		Assert.isTrue( blockpoints0.indexOf( 0x007F ) > -1 );
+		
+		var blockpoints1 = Seri.getBlock( 'Cyrillic' );
+		
+		Assert.equals( 256, blockpoints1.length );
+		Assert.isTrue( blockpoints1.indexOf( 0x0400 ) > -1 );
+		Assert.isTrue( blockpoints1.indexOf( 0x04FF ) > -1 );
+		
+		var blockpoints2 = Seri.getBlock( 'Ethiopic' );
+		
+		Assert.equals( 384, blockpoints2.length );
+		Assert.isTrue( blockpoints2.indexOf( 0x1200 ) > -1 );
+		Assert.isTrue( blockpoints2.indexOf( 0x137F ) > -1 );
+		
+		var blockpoints3 = Seri.getBlock( 'Unified Canadian Aboriginal Syllabics' );
+		
+		Assert.equals( 640, blockpoints3.length );
+		Assert.isTrue( blockpoints3.indexOf( 0x1400 ) > -1 );
+		Assert.isTrue( blockpoints3.indexOf( 0x167F ) > -1 );
 	}
 	
 }
