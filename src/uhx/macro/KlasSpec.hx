dbb5a9e713bee8ca048cafcee92210ecf7a596c7
Skial Bainn <skial.bainn@gmail.com>
2015-03-20 14:20:00 +0000

    I thought I committed this?!

---
+++ src/uhx/macro/KlasSpec.hx
@@ -0,0 +1,17 @@
+package uhx.macro;
+
+import utest.Assert;
+
+/**
+ * ...
+ * @author Skial Bainn
+ */
+class KlasSpec implements Klas {
+
+	public function new() {
+		
+	}
+	
+	public function testNothing() Assert.isTrue( true );
+	
+}
